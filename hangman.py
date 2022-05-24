8d052772ddb5bb1817810002ffe217fb318d9e64
Kingsley Ihemere <dekings44@gmail.com>
2022-05-24 19:28:08 +0100

    Hangman game first commit

---
+++ hangman.py
@@ -0,0 +1,32 @@
+#step 1
+
+import random
+
+from numpy import disp
+
+word_list = ['ardvark', 'baboon', 'camel']
+
+
+
+chosen_word = random.choice(word_list)
+
+print(f'The solution is {chosen_word}')
+
+
+display = []
+
+word_length = len(chosen_word)
+
+for letter in range(word_length):
+    display += "_"
+print(display)
+
+
+guess = input('Guess a letter: ').lower()
+
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+
+print(display)
