c4f317dc6e13ed6eb00967bbc7d7d948153bf50e
Kingsley Ihemere <dekings44@gmail.com>
2022-05-24 19:38:50 +0100

    Update in the Hangman game

--- hangman.py
+++ hangman.py
@@ -21,12 +21,18 @@ for letter in range(word_length):
     display += "_"
 print(display)
 
+end_of_game = False
+while not end_of_game:
+    guess = input('Guess a letter: ').lower()
 
-guess = input('Guess a letter: ').lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+    print(display)
 
-print(display)
+    if "_" not in display:
+        end_of_game = True
+
+    print('You Won')
