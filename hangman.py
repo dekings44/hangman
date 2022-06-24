43356b65ea800e80053e852bf85f597d4a5ba772
Kingsley Ihemere <dekings44@gmail.com>
2022-06-24 23:07:58 +0100

    update

--- hangman.py
+++ hangman.py
@@ -13,7 +13,7 @@ logo = art.logo
 word_list = words.word_list
 
 st.title(f'WELCOME TO HANGMAN GAME')
-print(f'WELCOME TO\n{logo}') 
+print(f'WELCOME TO\n{logo}\n') 
 
 print('INSTRUCTION:\n This game has 6 lives. for every time you guess the letter wrong, you help build the gallow')
 st.subheader('INSTRUCTION: The game has 6 lives. for every time you guess the letter wrong, you help build the gallow\n')
